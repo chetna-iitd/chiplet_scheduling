50a8468acd052cd168fd5405b86050d6347fcbec
chetna-iitd <112386942+chetna-iitd@users.noreply.github.com>
2025-06-16 10:42:21 +0200

    Add files via upload

---
+++ accelerator_1_exp3.m
@@ -0,0 +1,5 @@
+num_pes: 256
+l1_size_cstr: 10000
+l2_size_cstr: 300000
+noc_bw_cstr: 10
+offchip_bw_cstr: 10
---
+++ accelerator_2_exp2.m
@@ -0,0 +1,5 @@
+num_pes: 64
+l1_size_cstr: 10000
+l2_size_cstr: 300000
+noc_bw_cstr: 100000
+offchip_bw_cstr: 5000
